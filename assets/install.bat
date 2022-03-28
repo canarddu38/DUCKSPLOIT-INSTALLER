a58d9f7f2540b84f9d6ef81a768216b3bcdf24d4
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2022-03-28 13:49:51 +0200

    Update install.bat

--- assets/install.bat
+++ assets/install.bat
@@ -12,18 +12,17 @@ start firefox.exe https://github.com/canarddu38/DUCKSPLOIT/archive/root.zip
 :loop
 if exist C:\Users\%username%\Downloads\DUCKSPLOIT-root.zip (
 if not exist C:\Users\%username%\Downloads\*.part (
-taskkill /IM firefox.exe
-cd C:\temp
+taskkill /IM firefox.exe /f
+
 call unzip.exe C:\Users\%username%\Downloads\DUCKSPLOIT-root.zip
 echo [+] Ducksploit is downloaded!
 pause
 echo [~] Installing HACKER'S VERSION
-call C:\temp\DUCKSPLOIT-root\hacker\setup.lnk
+call DUCKSPLOIT-root\hacker\setup.lnk
 goto:eof
 goto loop
 )
-) 
-else (
+) else (
 goto loop
 )
 pause
